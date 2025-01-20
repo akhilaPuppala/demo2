f67b73780d7f6fb128f42cbee08cc0b37c8c1330
Yugandhar Reddy Allu <106137008+yugandhar123456789@users.noreply.github.com>
2025-01-20 15:30:44 +0530

    changed

--- src/main/java/com/Repository/MovieRepository.java
+++ src/main/java/com/Repository/MovieRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class MovieRepository extends JpaRepository {
 public interface MovieRepository extends JpaRepository<MovieEntity,Long> {
+
 }
