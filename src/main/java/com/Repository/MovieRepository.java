b0edf2ed907925ac64706186314f5261b46f82aa
vishrutth5214 <vishrutth5214@gmail.com>
2025-01-20 16:42:04 +0530

    Code has to be changed

--- src/main/java/com/Repository/MovieRepository.java
+++ src/main/java/com/Repository/MovieRepository.java
@@ -3,5 +3,5 @@ package com.Repository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class MovieRepository extends JpaRepository {
+public interface MovieRepository extends JpaRepository<Movie,Long> {
 }
