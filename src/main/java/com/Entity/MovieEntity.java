53c3cc02ec8bbdd3da9426d7a7d42c0b005ed081
Yugandhar Reddy Allu <106137008+yugandhar123456789@users.noreply.github.com>
2025-01-20 15:20:02 +0530

    addded entity

---
+++ src/main/java/com/Entity/MovieEntity.java
@@ -0,0 +1,5 @@
+package com.Entity;
+@Entity
+public class MovieEntity {
+    private LongId
+}
--- src/main/java/com/Repository/MovieRepository.java
+++ src/main/java/com/Repository/MovieRepository.java
@@ -1,7 +1,9 @@
 package com.Repository;
 
+import com.Entity.MovieEntity;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class MovieRepository extends JpaRepository {
+public interface MovieRepository extends JpaRepository<MovieEntity,Long> {
 }
